cab64a7ddc92712cf62f1fbe6b240f867bcd32fe
park-do-hyun <57884393a@gmail.com>
2022-12-01 11:17:09 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -16,16 +16,12 @@ class PostList(ListView):
 class PostDetail(DetailView) :
     model = Post
 
-# # def index(request):
-# #     posts = Post.objects.all().order_by('-pk')
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(Category=None).count
+        return context
 
-# #     return render(
-# #         request,
-# #         'blog/index.html',
-# #         {
-# #             'posts' : posts
-# #         }
-#     )
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
