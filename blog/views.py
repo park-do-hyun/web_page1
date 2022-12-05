b4d69ae7748a596183d59db7957139418b09a292
park-do-hyun <57884393a@gmail.com>
2022-12-05 10:40:33 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -52,3 +52,18 @@ def category_page(request, slug):
             'category': category,
         }
     )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag_page_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'tag' : tag,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(Category=None).count(),
+        }
+    )
